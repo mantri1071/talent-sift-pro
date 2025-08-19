c91f872928f59a9f2a4b97ccbf55c0e68e852041
Mantri Sumanth <mantrisumanth1@gmail.com>
2025-08-19 19:09:57 +0530

    fix

--- src/components/JobFormStep1.jsx
+++ src/components/JobFormStep1.jsx
@@ -6,7 +6,7 @@ import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Textarea } from '@/components/ui/textarea';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
-import ResumeMultiDropzoneStyled from '@/components/ResumeDropzoneStyled';
+import ResumeMultiDropzoneStyled from '@/components/ResumeMultiDropzoneStyled';
 import pic from '../pic.png';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css'; 
