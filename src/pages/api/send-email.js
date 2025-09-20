883ac72cc80b87407ff75a9fd4977ba4396a7680
Mantri Sumanth <mantrisumanth1@gmail.com>
2025-09-20 15:48:33 +0530

    development

--- src/pages/api/send-email.js
+++ src/pages/api/send-email.js
@@ -14,8 +14,8 @@ module.exports = async (req, res) => {
     }
 
     const msg = {
-      to: process.env.QNTRL_EMAIL,
-      from: process.env.FROM_EMAIL,
+      to: "768363363_30725000001300117@startitnow.mail.qntrl.com", // QNTRL email
+      from: "sumanth1mantri@gmail.com", // must be verified in SendGrid
       subject: `Shortlisted Candidate: ${name}`,
       text: `
 Candidate has been shortlisted.
